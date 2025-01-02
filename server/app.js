de20161f9857711f815c6f711593160008b472d8
saadhn4 <saadcfi4@gmail.com>
2025-01-02 20:17:40 +0530

    rate limiter added

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import gistsRouter from "./controllers/gists/index.js";
 import userRouter from "./controllers/user/index.js";
 import reposRouter from "./controllers/repos/index.js";
 import authMiddleware from "./middlewares/auth.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,17 @@ app.get("/", (req, res) => {
     res.status(500).json({ msg: error });
   }
 });
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 app.use("/api/public", publicRouter);
 app.use(authMiddleware);
